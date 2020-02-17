531800dec1324c55360dd77ab3d40b3be858a457
Matej The Tree <matejthetree@gmail.com>
2020-02-17 22:14:39 +0100

    intermediate lesson 1

--- intermediate/lesson_1/README.md
+++ intermediate/lesson_1/README.md
@@ -24,6 +24,7 @@ You will have task that include tutorials as well as custom tasks.
 
 1. Run basic flutter app on your device. It is a default counter app that is installed when you
     open a new project in Android Studio.
+    
 2. Check out Flutter Codelabs
 
    https://flutter.dev/docs/codelabs
@@ -31,10 +32,16 @@ You will have task that include tutorials as well as custom tasks.
    * Write Your First Flutter App, part 1
    * Write Your First Flutter App, part 2
    * Building Beautiful UIs with Flutter
+
 3. Start looking at the widget of the week playlist
    https://www.youtube.com/playlist?list=PLjxrf2q8roU23XGwz3Km7sQZFTdB996iG
 
 4. Read dart language tour
-
+   https://dart.dev/guides/language/language-tour
    Make sure to go trough it all, but dont expect to understand it all. It is a good place to have
    when you need to check some basic laguage features
+
+## Advanced homework
+
+1. https://git-scm.com/book/en/v2
+    first and second chapter
--- intermediate/lesson_1/column_row_button_card/lib/main.dart
+++ intermediate/lesson_1/column_row_button_card/lib/main.dart
@@ -48,6 +48,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -62,45 +67,60 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Yellow'),
+            color: Colors.yellow,
+            onPressed: ()=>print('yellow button'),
+          ),
+          RaisedButton(
+            child: Text('Orange'),
+            color: Colors.orange,
+            onPressed: ()=>print('orange button'),
+          ),
+          RaisedButton(
+            child: Text('Red'),
+            color: Colors.red,
+            onPressed: ()=>print('red button'),
+          ),
+        ],
+      ),
+      body: Card(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                color: Colors.pink.withOpacity(0.5),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Text('This is pink'),
+                ),
+              ),
+              Card(
+                color: Colors.blue.withOpacity(0.5),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Text('This is blue'),
+                ),
+              ),
+              Card(
+                color: Colors.green.withOpacity(0.5),
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Text('This is green'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
